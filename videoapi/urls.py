a34609cdfeef4c85052f300a3be55614d26cfaa4
Gerald__TM <taiyemusa1582@gmail.com>
2023-10-02 20:24:45 +0100

    updated my urls

--- videoapi/urls.py
+++ videoapi/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('upload/', views.upload_video_chunk, name='upload_video_chunk'),
-    path('list/', views.VideoList.as_view(), name='video-rec-list')
+    path('', views.VideoList.as_view(), name='video-rec-list')
     # path('transcribe/', views.transcribe_video, name='transcribe-video'),
 ]
