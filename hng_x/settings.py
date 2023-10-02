01a2473ea99c351d53560091e3afbbbe572b0307
Gerald__TM <taiyemusa1582@gmail.com>
2023-10-02 18:38:15 +0100

    updated settings

--- hng_x/settings.py
+++ hng_x/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['myrestapi.onrender.com','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['videoapi-0403.onrender.com','127.0.0.1', 'localhost']
 
 # Application definition
 
